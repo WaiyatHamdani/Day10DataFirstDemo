80baaa146038f9097fd51e07c6648fc200756b55
waiyat.hamdani <waiyat.hamdani@fdmgroup.com>
2020-09-25 15:16:46 -0400

    waiyatfirst commit

---
+++ Day10DatabaseFirstDemo/Day10DatabaseFirstDemo/ASKING/ASK.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10DatabaseFirstDemo.ASKING
+{
+    class ASK
+    {
+        public string AskQuestion(string question)
+        {
+            Console.WriteLine(question);
+            return Console.ReadLine();
+        }
+    }
+}
---
+++ Day10DatabaseFirstDemo/Day10DatabaseFirstDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using Day10DatabaseFirstDemo.ASKING;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10DatabaseFirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ASK a = new ASK();
+            using(var context = new WaiyatEntities()){  //database name
+                var user = new User()
+                {
+                    UserId = 1,
+                    Username = "Waiyat",
+                    Password = "Hamdani",
+                    Email = "waiyat@gmail.com"
+
+                };
+
+                context.Users.Add(user);
+                context.SaveChanges();
+            }
+        }
+    }
+}
